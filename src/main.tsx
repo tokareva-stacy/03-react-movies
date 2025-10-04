a9ac69a1798b947d8d15ca0ef1c2eee6f7fe33e1
tokareva-stacy <tokarevanyura@gmail.com>
2025-10-04 20:15:01 +0300

    fix app import

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from 'react-dom/client';
-import App from './components/APP/App.tsx';
+import App from './components/APP/App';
 import "modern-normalize";
 import './index.css';
 
